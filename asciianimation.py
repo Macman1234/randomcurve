b21e87c66b02b6b6d99a33793e3b0d0067ef6913
Mac Mansfield-Parisi <ninjamurloc@gmail.com>
2023-10-16 16:18:14 -0400

    recreate anim curve on terminal resize

--- asciianimation.py
+++ asciianimation.py
@@ -4,16 +4,24 @@ import cursorhide
 import os
 import atexit
 
-itersize = 1
-term_size = os.get_terminal_size()
-xsize = term_size.columns
-ysize = term_size.lines
-curve = mhcurve.mansfieldcurve(xsize,ysize)
-cursorhide.hide_cursor()
-atexit.register(cursorhide.show_cursor)
-while True:
-    curve.iterate(itersize)
-    print(curvetoascii.curvetoascii(curve),end="")
-    print("\r",end="")
-    print("\033[A"*(ysize+1),end="")
-    #time.sleep(0.1)
+if __name__ == "__main__":
+    itersize = 1
+    resizechecker = 20 # to avoid calling os.get_terminal_size on every iteration
+    cursorhide.hide_cursor()
+    atexit.register(cursorhide.show_cursor)
+    while True:
+        term_size = os.get_terminal_size()
+        xsize = term_size.columns
+        ysize = term_size.lines
+        curve = mhcurve.mansfieldcurve(xsize,ysize)
+        resize = False
+        resizecount = 0
+        while not resize:
+            resizecount += 1
+            if resizecount % resizechecker == 0:
+                resize = not (term_size == os.get_terminal_size())
+            curve.iterate(itersize)
+            print(curvetoascii.curvetoascii(curve),end="")
+            print("\r",end="")
+            print("\033[A"*(ysize+1),end="")
+            #time.sleep(0.1)
