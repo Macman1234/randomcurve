32ca6a5b3a4aa27dfc55ba2c30e1aed9d1257ccc
Maclean Mansfield-Parisi <ninjamurloc@gmail.com>
2023-10-25 16:20:24 -0400

    yeah i sent it pretty hard

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod mh_curve;
+
+mod text_curve_draw;
+use text_curve_draw::{make_curve_string,StyleKind};
+
+use std::env;
+
 fn main() {
-    let curve = mh_curve::MansfieldCurve::new(6,6);
-    for p in curve.path {
-        println!("{},{}",p.x,p.y);
-    }
+    let args: Vec<String> = env::args().collect();
+
+    let mut curve = mh_curve::MansfieldCurve::new(32,32);
+
+    curve.iterate(1000000);
+
+    println!("{}",make_curve_string(&curve, &StyleKind::Arc));
 }
